ac628e93f45266b380b82029c7270118cb903fed
syedshoaib18 <39904916+syedshoaib18@users.noreply.github.com>
2018-06-04 14:53:35 +0500

    Add files via upload
    Part 1 of the tutorial

---
+++ Part 1/webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: {
+      main: [
+        './src/main.js'
+        
+      ]
+  },
+  output: {
+    filename: './public/[name].js',
+
+  },
+
+module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }
+    ]
+  }
+
+}
